35fde3104e86da382a0a0d3c8c42b40a1f7d4514
Lukas Mateju <lmateju@umich.edu>
2026-02-12 13:29:32 -0500

    Input file formatting

--- src/config.py
+++ src/config.py
@@ -7,11 +7,22 @@ Global configuration parameters for the crew assignment model. All
 modafiable variables and input files are set and changed here.
 """
 
-# Input File
-INPUT_FILE = "dummy_file.csv"
+# Input Files
+IN_FLIGHTS = "dummy_file.csv"
+IN_AIRPORTS = "dummy_file.csv"
+IN_AIRPLANES = "dummy_file.csv"
 
 # Crew Behavior Settings
 MAX_SHIFT_HOURS = 11
 MIN_TRANSFER_TIME = 1
 ENFORCE_SAME_START_END = False
+MULTIPLE_AIRPLANE_TYPES = False
+MULTIPLE_CREW_TYPES = False
+# Other restriction ideas
+#  - Mandatory crew breaks (30 extra minutes without flight during shift)
+#  - Pilot certification restrictions (If using multiple crew types, pilots can only operate x types of planes)
+#  - Crew redundancy (If there will x number of crew that have to miss their shift, how many should be scheduled to avoid issues, ie. overload flight crews)
+#  - Balanced workload (Each crew member needs x number of hours at a minimum to be scheduled)
+#  - Allow deadheads (Crew members can fly be on flights without working to get back to orgional airport)
+#  - Multiday schedule (Add in international and overnight flights with additional crew restrictions)
 
